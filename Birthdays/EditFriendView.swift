9f0bd0530141fc0fbf3439fbbfe1570291d3e3f4
Era Shah <erashah001@gmail.com>
2025-04-27 23:22:20 -0400

    Added Update functionality

---
+++ Birthdays/EditFriendView.swift
@@ -0,0 +1,18 @@
+//
+//  EditFriendView.swift
+//  Birthdays
+//
+//  Created by Era Shah on 4/27/25.
+//
+
+import SwiftUI
+
+struct EditFriendView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditFriendView()
+}
